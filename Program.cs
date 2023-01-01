28744b5cec535e35cf9bde487b7638c61e795273
Adam Grover <adamggrover@gmail.com>
2023-01-01 12:53:37 +0000

    Refactor and add new statement before dice rolls

--- Program.cs
+++ Program.cs
@@ -7,17 +7,20 @@ namespace dice
         static void Main(string[] args)
         {
 
-            // initialise nuber of dice variable
+            // initialise number of dice variable
 
             int iNumOfDice = 0;
+
 
             // ask user how many dice they would like to roll
 
             Console.WriteLine("How many dice would you like to roll?");
 
+
             // assign user answer to number of dice variable// See https://aka.ms/new-console-template for more information
 
             iNumOfDice = Convert.ToInt16(Console.ReadLine()); 
+
 
             
 
@@ -33,6 +36,7 @@ namespace dice
             int iDiceRoll = random.Next(1, 6);
 
             // print the number of console
+            Console.WriteLine("You have rolled:");
             Console.WriteLine(iDiceRoll);
             }
 
